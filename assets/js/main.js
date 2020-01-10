1c6e6a4ca9e3a9a6ed161faa9adc57c3e76fc737
Andre J. Reid <axr490@miami.edu>
2020-01-10 13:30:05 -0500

    created 'doors' for SC trailer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,37 @@
+let leftDoor;
+let rightDoor;
+
 $(document).ready(function() {
+    CreateDoors();
+    OpenDoors();
 })
+
+let halfDisplay = $("#display")[0].getBoundingClientRect().width / 2;
+
+function CreateDoors() {
+    leftDoor = $("<div id='leftDoor'>");
+    leftDoor.css({
+        position: "absolute",
+        left: 0,
+        width: halfDisplay,
+        height: "100%",
+        "background-color": "red",
+        "border-right": "1px solid black"
+    });
+
+    $("#display").append(leftDoor);
+
+
+    rightDoor = $("<div id='rightDoor'>");
+    rightDoor.css({
+        position: "absolute",
+        left: halfDisplay,
+        width: halfDisplay,
+        height: "100%",
+        "background-color": "blue",
+        "border-left": "1px solid grey"
+    });
+
+    $("#display").append(rightDoor);
+
+}
