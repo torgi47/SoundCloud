5d391d2c95f184e414027e7e446c9bc6118d2217
ajreid <axr490@miami.edu>
2020-01-10 20:51:24 -0500

    added background and moving name

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ let tapeInterval;
 $(document).ready(function() {
     CreateDoors();
     DropTheTapes();
+    ThrowUpTheName();
     setTimeout(StopTheTapes, 40000);
+    // setTimeout(ThrowUpTheName, 20000);
 })
 
 let halfDisplay = $("#display")[0].getBoundingClientRect().width / 2;
@@ -60,23 +62,30 @@ function OpenDoors() {
 function DropTheTapes() {
     tapesDropping = true;
     let viewboxWidth = $("#viewbox")[0].getBoundingClientRect().width;
-    let tapeArea = viewboxWidth * 0.7;
+    let tapeArea = viewboxWidth * 0.6;
     let intervalFrequency = 500;
     
     tapeInterval = setInterval(function() {
 
         let randAngle = Math.floor(Math.random() * 360);
+        let randNum = Math.floor(Math.random() * 2);
+        if (randNum === 0) {randAngle *= -1;}
+
         let theTape = $("<img class='tape' src='assets/images/wavy tape.png'>");
         theTape.css({
             position: "absolute",
             top: -100,
-            left: (Math.floor(Math.random() * tapeArea)) + (viewboxWidth * 0.3),
+            left: (Math.floor(Math.random() * tapeArea)) + ((viewboxWidth - tapeArea) / 2),
             transform: `rotateZ(${randAngle}deg)`
         });
         $("#viewbox").append(theTape);
 
+        if (randNum === 0) {
+            theTape.css("animation-direction", "reverse");
+        }
+
         theTape.animate({
-            top: $("#viewbox")[0].getBoundingClientRect().height
+            top: $("#viewbox")[0].getBoundingClientRect().height + 100
         }, {
             easing: "linear",
             duration: 3000,
@@ -92,4 +101,17 @@ function DropTheTapes() {
 function StopTheTapes() {
     clearInterval(tapeInterval);
     tapesDropping = false;
+}
+
+function ThrowUpTheName() {
+
+    let prophetTop = $("#viewbox")[0].getBoundingClientRect().height * 0.7;
+    prophetTop = -100;
+
+    $("#prophet").animate({
+        top: prophetTop
+    }, {
+        duration: 15000
+    })
+
 }
